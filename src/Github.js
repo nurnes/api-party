c93f69e47b97c10ef7a0da002f17ae2d58496d41
Naomi Urnes <nurnes@purdue.edu>
2017-06-20 11:59:11 -0400

    Add GitHub user component

--- src/Github.js
+++ src/Github.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import './Github.css'
 import {Route} from 'react-router-dom'
+import User from './User'
 
 class Github extends Component {
     state = {
@@ -12,15 +13,21 @@ class Github extends Component {
         this.setState({ username })
     }
 
+    handleSubmit = (ev) => {
+        ev.preventDefault()
+        this.props.history.push(`/github/${this.state.username}`)
+    }
+
     render() {
         return (
             <div className="github">
                 <img src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png" alt="GitHub logo" className="github-logo" />
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <input type="text" value={this.state.username} onChange={this.handleChange} />
                     <button type="submit">Look up Github user</button>
                 </form>
                 <Route exact path='/github' render={() => <h3>Please enter a username to search on GitHub</h3>} />
+                <Route path='/github/:user' component={User} />
             </div>
         )
     }
---
+++ src/User.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class User extends Component {
+    render () {
+        return(
+            <p>a</p>
+        )
+    }
+}
+
+export default User
