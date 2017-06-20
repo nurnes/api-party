326b44d8f9a2576575bf83c46e57acf47dc96b8d
Naomi Urnes <nurnes@purdue.edu>
2017-06-20 11:39:50 -0400

    Image and form in Github component

--- src/Github.js
+++ src/Github.js
@@ -1,8 +1,17 @@
 import React, {Component} from 'react'
+import './Github.css'
 
 class Github extends Component {
     render() {
-        return <h1>Github</h1>
+        return (
+            <div className="github">
+                <img src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png" alt="GitHub logo" className="github-logo" />
+                <form>
+                    <input type="text" />
+                    <button type="submit">Look up Github user</button>
+                </form>
+            </div>
+        )
     }
 }
 
