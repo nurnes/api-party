93851868dd2e01035cd1278c62b448e19180a6bf
Naomi Urnes <nurnes@purdue.edu>
2017-06-20 12:12:30 -0400

    GitHub API fetch (once)

--- src/User.js
+++ src/User.js
@@ -1,9 +1,37 @@
 import React, {Component} from 'react'
 
 class User extends Component {
+    state = {
+        user: {
+            avatar_url: '',
+            login: '',
+            followers: '',
+            following: '',
+            location: '',
+            html_url: ''
+        }
+    }
+    constructor(props){
+        super(props)
+        this.fetchData()
+    }
+
+    fetchData(){
+        fetch(`https://api.github.com/users/${this.props.match.params.user}`)
+            .then(response => response.json())
+            .then(user => this.setState({ user }))
+    }
     render () {
+        const {user} = this.state
         return(
-            <p>a</p>
+            <div>
+                <img src={user.avatar_url} />
+                <h2>{user.login}</h2>
+                <h3>Followers: {user.followers}</h3>
+                <h3>Following: {user.following}</h3>
+                <h3>Location: {user.location}</h3>
+                <a href={user.html_url} target="_">a</a>
+            </div>
         )
     }
 }
