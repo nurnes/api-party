e4a8204e061abaa9e99f86ebf5f7c4f67185d44c
Naomi Urnes <nurnes@purdue.edu>
2017-06-20 11:32:32 -0400

    NavLinks/style

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Switch, Route, NavLink} from 'react-router-dom'
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h3>Ain't no party like an</h3>
-          <h1>API Party</h1>
+          <div className="App-headings">
+            <h3>Ain't no party like an</h3>
+              <h1>API Party</h1>
+            </div>
+            <ul className="nav-links">
+              <li>
+                <NavLink to='/github'>Github API</NavLink>
+              </li>
+            </ul>
+          </div>
+          <Switch>
+            <Route path='/github' render={() => <h1>Github!</h1>} />
+            <Route render={() => <p>To get started, click one of the links above</p>} />
+          </Switch>
         </div>
-      </div>
     );
   }
 }
-
 export default App;
